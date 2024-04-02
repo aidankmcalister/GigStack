f46c8a58f48636f43a628c9e5e7be80bce502a24
Aidan <aidankmcalister@gmail.com>
2024-04-02 14:29:13 -0500

    attending gigs cell added

--- web/src/pages/AttendingGigsPage/AttendingGigsPage.tsx
+++ web/src/pages/AttendingGigsPage/AttendingGigsPage.tsx
@@ -1,5 +1,6 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import AttendingGigsCell from 'src/components/AttendingGigsCell'
 
 const AttendingGigsPage = () => {
   return (
@@ -7,14 +8,7 @@ const AttendingGigsPage = () => {
       <Metadata title="AttendingGigs" description="AttendingGigs page" />
 
       <h1>AttendingGigsPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/AttendingGigsPage/AttendingGigsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>attendingGigs</code>, link to me with `
-        <Link to={routes.attendingGigs()}>AttendingGigs</Link>`
-      </p>
+      <AttendingGigsCell />
     </>
   )
 }

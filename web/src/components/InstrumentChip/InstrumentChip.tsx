68c191b44b73aba37110ae1804b25dc74d93c5ca
Aidan <aidankmcalister@gmail.com>
2024-04-02 17:21:28 -0500

    color change

--- web/src/components/InstrumentChip/InstrumentChip.tsx
+++ web/src/components/InstrumentChip/InstrumentChip.tsx
@@ -1,6 +1,6 @@
 const InstrumentChip = ({ instrument }: { instrument: string }) => {
   return (
-    <li className="rounded-md border border-main-blue px-1.5 text-main-blue shadow-sm">
+    <li className="rounded-md border border-main-orange/80 px-1.5 text-main-orange/80 shadow-sm">
       {instrument
         .split(' ')
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))

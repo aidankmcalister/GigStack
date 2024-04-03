3edc69795b1f2b7cb19eb22587524b17dd33ced5
Aidan <aidankmcalister@gmail.com>
2024-04-03 00:14:00 -0500

    button styling changed

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 const Button = ({ className = '', onClick, children }) => {
   return (
     <button
-      className={`rounded-md border px-2 py-1 ${className}`}
+      className={`rounded-md border bg-main-orange px-2 py-1 font-medium text-white hover:bg-main-orange/70 ${className}`}
       onClick={onClick}
     >
       {children}
